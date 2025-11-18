04b06d9f034332a414876ac0b64c17ef2064ca6f
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-11-18 17:26:50 -0500

    feat: add countdown loop exercise for Chapter 5 intro_to_looping.py

---
+++ projects/chapter5/intro_to_looping.py
@@ -0,0 +1,10 @@
+number = input("Please enter in a starting number: ")
+
+start_number = int(number)
+
+print("Counting down...")          
+
+for i in range(start_number, 0, -1):
+    print(f"the current number is: {i}")
+
+print("Blast off!")
