1c424062d7f283f2e4a13c52d3716ebf6dd734c1
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-12-02 18:47:42 -0500

    feat: added iterating_over_range exercise for chapter5

---
+++ projects/chapter5/iterating_over_range.py
@@ -0,0 +1,10 @@
+# Iterating Over a Range — Lighthouse Labs Exercise
+
+start_number = int(input("Enter a starting number: "))
+
+print("Counting down...")
+
+for i in range(start_number, 0, -1):
+    print(i)
+
+print("Blast off!")
