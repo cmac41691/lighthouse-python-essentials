b86ecfbe81d9688214aa93c9c333e59e8d76da6d
Coady Maclellan <156689197+cmac41691@users.noreply.github.com>
2025-12-13 09:01:37 -0500

    feat: update exercise_function.py with celsius_to_fahrenheit, is_even and repeat_text

--- projects/chapter6/parameters_and_return_values.py
+++ projects/chapter6/parameters_and_return_values.py
@@ -1,22 +1,36 @@
-def add_number():
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-    
-    result = num1 + num2
-    print(f"The total is {result}")
+# celsius_to_fahrenheit
+def celsius_to_fahrenheit():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"That temperature in Fahrenheit is: {fahrenheit}")
+    return fahrenheit
+
+
+# is_even
+def is_even():
+    even_num = input("Enter a number: ")
+    number = int(even_num)
+
+    if number % 2 == 0:
+        print(f"{number} is even.")
+        return True
+    else:
+        print(f"{number} is not even.")
+        return False
+
+
+# repeat_text
+def repeat_text():
+    text = input("Enter a string to repeat: ")
+    repeat = 4   # or: int(input("How many times? "))
+    result = text * repeat
+    print(result)
     return result
 
 
-def calculate_area():
-    base = int(input("Enter the base: "))
-    height = int(input("Enter the height: "))
+# --- CALL THE FUNCTIONS --- 
+celsius_to_fahrenheit()
+is_even()
+repeat_text()
 
-    area = (base * height) / 2
-    print(f"The area is {area}")
-    return area
-
-
-# call the functions to test them
-add_number()
-calculate_area()
  
